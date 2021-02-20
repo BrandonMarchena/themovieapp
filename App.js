3eaa8811cd4b47c92beaa00134f1b19dc47c3c61
BrandonMarchena <brandon.marchena@aplinet.co>
2021-02-20 01:36:39 -0500

    Proyecto themovieapp

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
